5785e3a7c0dc1f29a9d4db0d9c4daf1ae265c5ec
Truc Trinh <ttrinh2@umbc.edu>
2022-06-21 05:28:10 -0400

    Assignment 4 completed

---
+++ practicejava/src/main/java/com/assignment/ProgramPatternNumberPyramid.java
@@ -0,0 +1,39 @@
+/* Assignment 4: Print the following pattern
+1
+212
+32123
+4321234
+543212345
+ */
+
+package com.assignment;
+
+import java.util.Scanner;
+
+public class ProgramPatternNumberPyramid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n, a;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter n values: ");
+		n = input.nextInt();
+		input.close();
+		a = n;
+		
+		for(int i=1; i<=n; i++) {
+			// for(int j=a; j>1; j--) { System.out.print(" "); }
+			for(int k = i; k!=0; k--) {
+				System.out.print(k);
+			}
+			a--;
+			for(int l = 2; l<=i; l++) {
+				System.out.print(l);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
